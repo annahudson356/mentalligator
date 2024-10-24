990dfa56497c4a1cde38ebc61b1e14caa9d747d1
Jayden Spitulnik <81199186+JSS874@users.noreply.github.com>
2024-10-24 09:54:13 -0400

    sign up and login authentication

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,34 +1,50 @@
 import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { UserService } from '../services/user.service';
 import { User } from '../entities/user.entity';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
-    @Get()
+    @Get('/all')
     findAll(): Promise<User[]> {
         return this.userService.findAll();
     }
 
-    @Get(':id')
+    @Get('by-id/:id')
     findOne(@Param('id') id: number): Promise<User> {
         return this.userService.findOne(id);
     }
 
-    @Get(":email")
+    @Get("by-email/:email")
     findOneByEmail(@Param('email') email: string): Promise<User> {
         return this.userService.findOneByEmail(email);
     }
 
-    @Get(":name")
+    @Get("by-name/:name")
     findOneByName(@Param('name') name: string): Promise<User> {
         return this.userService.findOneByName(name);
     }
 
     @Post()
-    create(@Body() user: User): Promise<User> {
-        return this.userService.create(user);
+    async create(@Body() user: User): Promise<User> {
+        try {
+            return await this.userService.create(user);
+        }
+        catch (e) {
+            console.error(e);
+            throw new HttpException({
+                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                error: 'Error creating user: ' + e.message,
+            }, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Get('sign-in/:email/:password')
+    signIn(@Param('email') email: string, @Param('password') password: string): Promise<any> {
+        console.log("sign-in" + email + password);
+        return this.userService.validate(email, password);
     }
 
     @Delete(':id')
