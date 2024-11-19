94ca595105f24c6dad9d2264a8119fe746e231ff
logand9497 <logand@rangerweb.org>
2024-11-19 02:20:49 -0500

    UI tweaks

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
-import Main from './Main';
+import Main from './main';
 import reportWebVitals from './reportWebVitals';
 
 const App = () => {
